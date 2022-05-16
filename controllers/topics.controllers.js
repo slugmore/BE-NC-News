7c3db222f601a406fef756d7cd20582f9790310f
Jack Kelsey <Jack@Admins-iMac.lan>
2022-05-16 16:20:25 +0100

    Added catch block for errors on controller

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -5,6 +5,9 @@ getTopics = (req, res) => {
     fetchTopics().then((topics) => {
         res.status(200).send({topics: topics})
     })
+    .catch((err) => {
+        console.log(err);
+    });
    
 }
 
